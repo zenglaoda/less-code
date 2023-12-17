d293a76820514b320349e84bf13552f0d6c4a334
zengweifu <814595325@qq.com>
2023-12-17 22:53:42 +0800

    data

--- src/workspace/entities.ts
+++ src/workspace/entities.ts
@@ -10,6 +10,26 @@ export const formElements = [
     },
   },
   {
+    type: 1,
+    configuration: {
+      placeholder: '请输入昵称',
+      label: '昵称',
+      field: 'nickname',
+      maxlength: 10,
+      clearable: true
+    },
+  },
+  {
+    type: 1,
+    configuration: {
+      placeholder: '请输入住址',
+      label: '住址',
+      field: 'address',
+      maxlength: 10,
+      clearable: true
+    },
+  },
+  {
     type: 2,
     configuration: {
       placeholder: '请输入生日',
@@ -17,5 +37,23 @@ export const formElements = [
       format: 'YYYY/MM/DD',
       valueFormat: 'YYYY/MM/DD',
     },
-  }
+  },
+  {
+    type: 2,
+    configuration: {
+      placeholder: '请输入开始时间',
+      label: '开始时间',
+      format: 'YYYY/MM/DD',
+      valueFormat: 'YYYY/MM/DD',
+    },
+  },
+  {
+    type: 2,
+    configuration: {
+      placeholder: '请输入结束时间',
+      label: '结束时间',
+      format: 'YYYY/MM/DD',
+      valueFormat: 'YYYY/MM/DD',
+    },
+  },
 ]
